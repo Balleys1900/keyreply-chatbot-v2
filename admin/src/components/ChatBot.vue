53b1ef109252a673b322504dce4f60abf1f16c4f
khaphan2806 <khaphan2806@gmail.com>
2021-09-04 18:31:08 +0700

    Update dashboard UI

--- admin/src/components/ChatBot.vue
+++ admin/src/components/ChatBot.vue
@@ -1,11 +1,46 @@
 <template>
-  <div class="">Chat Bot</div>
+  <div class="chat-bot">
+    <div class="action">
+      <el-button type="primary" size="small" @click="openModal">
+        <i class="el-icon-edit"></i> Add
+      </el-button>
+      <chat-bot-modal ref="chatFormModal" />
+    </div>
+
+    <div class="table">
+      <chat-bot-table />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/runtime-core';
+import { defineComponent, ref } from '@vue/runtime-core';
+import ChatBotModal from './ChatBotModal.vue';
+import ChatBotTable from './ChatBotTable.vue';
 
-export default defineComponent({});
+export default defineComponent({
+  components: { ChatBotTable, ChatBotModal },
+  setup() {
+    const dialogVisible = ref(false);
+
+    return {
+      dialogVisible
+    };
+  },
+  methods: {
+    openModal() {
+      (this.$refs.chatFormModal as any).open();
+    }
+  }
+});
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+i {
+  margin-right: 5px;
+}
+
+.table {
+  margin-top: 20px;
+}
+</style>
---
+++ admin/src/components/ChatBotForm.vue
@@ -0,0 +1,235 @@
+<template>
+  <el-form
+    label-position="top"
+    :model="formData"
+    ref="formChatBot"
+    label-width="120px"
+    :rules="rules"
+  >
+    <el-row :gutter="10">
+      <el-col :span="12">
+        <el-form-item label="Chat Name" prop="name">
+          <el-input v-model="formData.name" placeholder="Ex: conversation_start..."></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="12">
+        <el-form-item label="Chat Text" prop="text">
+          <el-input v-model="formData.text" placeholder="Ex: Welcome to my store..."></el-input>
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <el-row>
+      <span class="form-label">Chat buttons</span>
+
+      <el-col :span="24" class="button-group">
+        <!-- Button Item -->
+        <transition-group name="list">
+          <div
+            class="button-item list-item "
+            v-for="(button, index) in formData.buttons"
+            :key="button.id"
+          >
+            <div class="button-title">
+              <span>Button {{ ++index }}</span>
+              <span class="close-button" @click="deleteButton(button.id)">
+                <i class="el-icon-close"></i>
+              </span>
+            </div>
+            <el-form-item label="Button Text">
+              <el-input v-model="button.text"></el-input>
+            </el-form-item>
+            <el-row :gutter="10">
+              <el-col :span="12">
+                <el-form-item label="Button Event">
+                  <el-select
+                    v-model="button.event"
+                    placeholder="Select button event..."
+                    style="width:100%"
+                  >
+                    <el-option
+                      v-for="item in buttonEvents"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value"
+                    >
+                    </el-option>
+                  </el-select>
+                </el-form-item>
+              </el-col>
+
+              <el-col :span="12">
+                <el-form-item label="Button Data">
+                  <el-select
+                    v-model="button.data"
+                    placeholder="Select button data..."
+                    style="width:100%"
+                  >
+                    <el-option
+                      v-for="item in nodeDatas"
+                      :key="item.value"
+                      :label="item.label"
+                      :value="item.value"
+                    >
+                    </el-option>
+                  </el-select>
+                </el-form-item>
+              </el-col>
+            </el-row>
+          </div>
+        </transition-group>
+        <!-- End Button Item -->
+
+        <!-- Add Button -->
+        <div style="textAlign:center ;">
+          <el-button type="primary" plain @click="addButton">
+            <i class="el-icon-plus"></i> Add Button
+          </el-button>
+        </div>
+        <!-- End Add Button -->
+      </el-col>
+    </el-row>
+  </el-form>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue';
+
+export default defineComponent({
+  props: ['lang'],
+  setup() {
+    interface FormButton {
+      id: string;
+      text: string;
+      event: string;
+      data: string;
+    }
+
+    interface FormData {
+      name: string;
+      text: string;
+      buttons: FormButton[];
+    }
+
+    const rules = {
+      name: [{ required: true, message: 'Please input chat name.', trigger: 'blur' }],
+      text: [{ required: true, message: 'Please input chat text.', trigger: 'blur' }]
+    };
+
+    const formData: FormData = reactive({
+      name: '',
+      text: '',
+      buttons: []
+    });
+
+    const buttonEvents = [
+      {
+        label: 'Capture',
+        value: 'capture'
+      },
+      {
+        label: 'Goto',
+        value: 'goto'
+      }
+    ];
+
+    const nodeDatas = [
+      {
+        label: 'Node 1',
+        value: 'node1'
+      },
+      {
+        label: 'Node 2',
+        value: 'node2'
+      }
+    ];
+
+    const getFormData = () => {
+      return formData;
+    };
+
+    const addButton = () => {
+      formData.buttons.push({
+        id: (formData.buttons.length + 1).toString(),
+        text: '',
+        event: '',
+        data: ''
+      });
+    };
+
+    const deleteButton = (id: string) => {
+      formData.buttons = formData.buttons.filter((button) => button.id != id);
+    };
+
+    return {
+      formData,
+      buttonEvents,
+      getFormData,
+      nodeDatas,
+      addButton,
+      deleteButton,
+      rules
+    };
+  },
+  methods: {
+    submitForm(formName: string) {
+      (this.$refs[formName] as any).validate((valid: boolean) => {
+        if (valid) {
+          return this.formData;
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.button-group {
+  padding: 20px;
+  border: 1px solid #eee;
+
+  .button-item {
+    padding: 20px;
+    margin-bottom: 20px;
+    border: 1px solid #69c0ff;
+  }
+}
+.form-label {
+  margin-bottom: 20px;
+}
+
+.button-title {
+  text-align: center;
+  text-transform: uppercase;
+  color: #40a9ff;
+  position: relative;
+
+  .close-button {
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    cursor: pointer;
+    color: rgb(165, 163, 163);
+
+    &:hover {
+      color: #40a9ff;
+    }
+  }
+}
+
+.list-item {
+  margin-right: 10px;
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+</style>
