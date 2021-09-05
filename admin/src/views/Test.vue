30a6c04481500c19cdeb2e6e6759b157719e525a
Balleys1900 <leminhtai325@gmail.com>
2021-09-05 11:19:45 +0700

    Test authenticate with auth0

---
+++ admin/src/views/Test.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Test Auth</h1>
+    <p>{{ token }}</p>
+  </div>
+</template>
+
+<script>
+import { inject } from 'vue';
+export default {
+  setup() {
+    //    isAuthenticated: computed(() => state.isAuthenticated),
+    // loading: computed(() => state.loading),
+    // user: computed(() => state.user),
+    // getIdTokenClaims,
+    // getTokenSilently,
+    // getTokenWithPopup,
+    // handleRedirectCallback,
+    // loginWithRedirect,
+    // loginWithPopup,
+    // logout
+    const auth0 = inject('Auth');
+    console.log(auth0);
+    const token = auth0.getTokenSilently();
+    token.then((res) => console.log(res));
+
+    console.log(token);
+    return { token };
+  }
+};
+</script>
+
+<style></style>
