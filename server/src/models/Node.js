6fe4b8117ea7efbd2fc7a5a952b146527f441009
khaphan2806 <khaphan2806@gmail.com>
2021-09-06 16:02:51 +0700

    fix BE

--- server/src/models/Node.js
+++ server/src/models/Node.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Language = require('./Language');
 
 const NodeSchema = new mongoose.Schema({
-    id:String,
-    language:[Language],
+  name: String,
+  language: [Language],
 });
 
-const Node= mongoose.model('node',NodeSchema);
+const Node = mongoose.model('node', NodeSchema);
 module.exports = NodeSchema;
--- server/src/types/typeDefs.js
+++ server/src/types/typeDefs.js
@@ -1,81 +1,80 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type Button {
-        text: String
-        event:String
-        data:String
-    }
-    
-    type Condition {
-        property:String
-        value:String
-        operator:String
-    }
-    
-     type Language {
-        lang:String
-        buttons:[Button]
-        conditions:[Condition]
-        text:String
-        regex:String
-    }
-    
-    type Node {
-    id:String,
-    language:[Language],
-    }
-   
-    type Content {
-        content:[Node]
-        version:String
-        description:String
-    }
-    
-    
-    input ButtonInput {
-        text: String
-        event:String
-        data:String
-    }
-    
-    input ConditionInput {
-        property:String
-        lang:String
-        operator:String
-    }
-    
-    input LanguageInput {
-        lang:String
-        buttons:[ButtonInput]
-        text:String
-    }
-    
-    input NodeInput {
-       id:String
-       language:[LanguageInput]
-    }
-    
-    input ContentInput {
-       content:[NodeInput]
-       version:String
-       description:String
-    }
-    
-    type Notification {
+  type Button {
+    text: String
+    event: String
+    data: String
+  }
+
+  type Condition {
+    property: String
+    value: String
+    operator: String
+  }
+
+  type Language {
+    lang: String
+    buttons: [Button]
+    conditions: [Condition]
+    text: String
+    regex: String
+  }
+
+  type Node {
+    name: String
+    language: [Language]
+  }
+
+  type Content {
+    content: [Node]
+    version: String
+    description: String
+  }
+
+  input ButtonInput {
+    text: String
+    event: String
+    data: String
+  }
+
+  input ConditionInput {
+    property: String
+    lang: String
+    operator: String
+  }
+
+  input LanguageInput {
+    lang: String
+    buttons: [ButtonInput]
+    text: String
+  }
+
+  input NodeInput {
+    id: String
+    language: [LanguageInput]
+  }
+
+  input ContentInput {
+    content: [NodeInput]
+    version: String
+    description: String
+  }
+
+  type Notification {
     message: String
-    }
-    
-    type Query{
+  }
+
+  type Query {
     getContentById(id: ID): Content
     getAll: [Content]
-    }
-    
-    type Mutation {
+  }
+
+  type Mutation {
     createContent(dto: ContentInput): Content
-    updateContent(id:ID ,content: ContentInput): Content
-    deleteContent(id:ID ): Notification
-    }
+    updateContent(id: ID, content: ContentInput): Content
+    deleteContent(id: ID): Notification
+  }
 `;
 
 module.exports = typeDefs;
