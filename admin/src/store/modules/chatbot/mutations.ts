cad364c80f5186354346c9f2e9998e3933926bd0
khaphan2806 <khaphan2806@gmail.com>
2021-09-09 19:24:14 +0700

    Add update function v1

--- admin/src/store/modules/chatbot/mutations.ts
+++ admin/src/store/modules/chatbot/mutations.ts
@@ -4,5 +4,19 @@ export default {
   },
   SET_EDIT_NAME(state: any, payload: any) {
     state.editName = payload;
+  },
+  UPDATE_CHATBOT_DATA(state: any, payload: any) {
+    const chatbotData = [...state.chatbotData];
+
+    console.log(chatbotData);
+
+    const newChatbotData = chatbotData.map((item: any) => {
+      if (item.name === payload.name) {
+        item = { ...item, language: payload.language };
+      }
+      return item;
+    });
+
+    state.chatbotData = newChatbotData;
   }
 };
